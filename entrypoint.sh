82d5fdc8fe31b8549b2e123fdf457a9549e83245
Yeongcheol Kim <aree91@naver.com>
2022-02-21 15:53:46 +0900

    Printed progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,28 @@
 # /entrypoint.sh
 #!/bin/sh
 
-
+echo ==========Authenticating==========
 echo $TOKEN > token
 gh auth login --with-token < token
+echo ==================================
 
+echo ==========Cloning $GITHUB_REPOSITORY==========
 gh repo clone $GITHUB_REPOSITORY repo_clone
 cd repo_clone
+echo ==================================
 
 DESTINATION_BRANCH="$GITHUB_REF_NAME"
 SOURCE_BRANCH="$GITHUB_REF_NAME-auto-patch-$(date +%s%N)"
 echo $DESTINATION_BRANCH
 echo $SOURCE_BRANCH
 
+echo ==========Setting Remote==========
 git remote set-url origin "https://$GITHUB_ACTOR:$TOKEN@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY"
 git fetch origin '+refs/heads/*:refs/heads/*' --update-head-ok
 git --no-pager branch -a -vv
+echo ==================================
 
+echo ==========Pushing Commit==========
 git checkout $GITHUB_REF_NAME
 git checkout -b $SOURCE_BRANCH
 # generate dummy
@@ -24,6 +30,7 @@ echo dummy > dummy
 git add .
 git commit -m "Auto-generated"
 git push origin $SOURCE_BRANCH
+echo ==================================
 
 
 COMMAND="gh pr create \
